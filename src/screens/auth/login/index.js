df16ff7b345f66d3e94e4cbe538dabe64f36f597
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-04-16 01:10:36 -0300

    Register, login and home page are nice

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Header from '../../../components/header';
+import('../../../styles/register.scss')
 
-
-const login = () => {
+const register = () => {
   return (
-    <h1>login</h1>
+    <body className='bodyregister'>
+      <Header />
+      <section className='sectionregister'>
+        <div className='divregister'>
+          <p className='pregister'>Sign in</p>
+          <div className='divformregister'>
+            <form className='formregister'>
+              <div class="field">
+                <label class="label labelregister">Email</label>
+                <div class="control">
+                  <input class="input inputregister" type="text" />
+                </div>
+              </div>
+              <div class="field">
+                <label class="label labelregister">Password</label>
+                <div class="control">
+                  <input class="input inputregister" type="password" />
+                </div>
+              </div>
+              <button class="button buttonregister is-large">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </body>
   )
 }
-export default login;
+export default register;
